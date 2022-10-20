ce846670b6c49254785a7eceb606dbb08e63b4ed
b-van-b <115096508+b-van-b@users.noreply.github.com>
2022-10-20 17:52:12 +0000

    feat: accept new users

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// user schema and model
+const userSchema = new mongoose.Schema({
+  username: String,
+  exercises: [
+    {
+      description: String,
+      duration: Number,
+      date: Date,
+    },
+  ],
+});
+const User = mongoose.model("User", userSchema);
+
 app.use(cors());
 app.use(express.static("public"));
+app.use("/api/users", bodyParser.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/api/users", (req, res) => {
+  const name = req.body.username.trim();
+  if (!name) {
+    return console.log("Empty string submitted as username");
+  }
+  console.log(
+    "\nNew user submitted: " + name + "\n- Inserting new document..."
+  );
+  user = new User({ username: req.body.username });
+  user.save((err, data) => {
+    if (err) return console.log(err);
+    console.log("- Success:\n" + data);
+    res.json({ username: data.username, _id: user._id });
+  });
+});
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  console.log("\nNew exercise data submitted...");
+  console.log("- params: " + req.params);
+  console.log("- body: " + req.body);
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
