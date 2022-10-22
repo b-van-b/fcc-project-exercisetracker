18c997d7d9c6ff090a0a4b9318e5349623690daf
b-van-b <115096508+b-van-b@users.noreply.github.com>
2022-10-22 16:31:35 +0000

    refactor: handle createUser, findUser, findAllUsers, createExercise within database.js

--- database.js
+++ database.js
@@ -9,14 +9,76 @@ mongoose.connect(process.env.MONGO_URI, {
 // user schema and model
 const userSchema = new mongoose.Schema({
   username: String,
-  exercises: [
-    {
-      description: String,
-      duration: Number,
-      date: Date,
-    },
-  ],
 });
 const User = mongoose.model("User", userSchema);
 
-exports.User = User;
+// exercise schema and model
+const exerciseSchema = new mongoose.Schema({
+  user_id: String,
+  description: String,
+  duration: Number,
+  date: Date,
+});
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+const createUser = (username, done) => {
+  if (!username) {
+    return { error: "empty username submitted; no user created" };
+  }
+  console.log(`\nNew user submitted: ${username}\n- Inserting new document...`);
+  user = new User({ username: username });
+  user.save((err, data) => {
+    if (err) return console.log(err);
+    console.log("- Success:\n" + data);
+    done(null, data);
+  });
+};
+
+const findUser = (userId, done) => {
+  User.findById(userId, (err, data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  });
+};
+
+const findAllUsers = (done) => {
+  User.find()
+    .select({ _id: 1, username: 1 })
+    .exec((err, data) => {
+      if (err) return console.log(err);
+      done(null, data);
+    });
+};
+
+const createExercise = (userId, description, duration, date, done) => {
+  // create an exercise doc and return the exercise with user id and username
+  const exercise = new Exercise({
+    user_id: userId,
+    description: description,
+    duration: Number(duration),
+    date: date,
+  });
+  console.log("Inserting new exercise: \n" + exercise);
+  // make sure the user exists, and get the username
+  User.findById(userId, (err, user) => {
+    if (err) return console.log(err);
+    if (!user) return done(null, {});
+    // if user exists, add the exercise
+    exercise.save((err, savedata) => {
+      if (err) return console.log(err);
+      // return exercise with additional user info
+      done(null, {
+        _id: userId,
+        username: user.username,
+        description: savedata.description,
+        duration: savedata.duration,
+        date: savedata.date,
+      });
+    });
+  });
+};
+
+exports.createUser = createUser;
+exports.findAllUsers = findAllUsers;
+exports.findUser = findUser;
+exports.createExercise = createExercise;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
-const { User } = require("./database");
+const database = require("./database");
 require("dotenv").config();
 
 app.use(cors());
@@ -12,118 +12,93 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/users", (req, res) => {
-  User.find()
-    .select({ _id: 1, username: 1 })
-    .exec((err, data) => {
-      if (err) return console.log(err);
-      res.json(data);
-    });
-});
-
 app.post("/api/users", (req, res) => {
   const name = req.body.username.trim();
-  if (!name) {
-    return console.log("Empty string submitted as username");
-  }
-  console.log(
-    "\nNew user submitted: " + name + "\n- Inserting new document..."
-  );
-  user = new User({ username: req.body.username });
-  user.save((err, data) => {
+  database.createUser(name, (err, data) => {
     if (err) return console.log(err);
-    console.log("- Success:\n" + data);
-    res.json({ username: data.username, _id: user._id });
+    res.json(data);
+  });
+});
+
+app.get("/api/users", (req, res) => {
+  database.findAllUsers((err, data) => {
+    if (err) return console.log(err);
+    res.json(data);
   });
 });
 
 app.post("/api/users/:_id/exercises", (req, res) => {
   console.log("\nNew exercise data submitted...");
   console.log("- user id: " + req.params._id);
-  const exercise = {
-    description: req.body.description,
-    duration: Number(req.body.duration),
-    date: req.body.date,
-  };
-  console.log(exercise);
-  User.findByIdAndUpdate(
+
+  database.createExercise(
     req.params._id,
-    { $push: { exercises: exercise } },
-    { new: true },
+    req.body.description,
+    req.body.duration,
+    req.body.date,
     (err, data) => {
       if (err) return console.log(err);
-      if (!data) return console.log("- No such user!");
-      // return expected output
-      const lastExercise = data.exercises[data.exercises.length - 1];
-      const ans = {
-        _id: req.params._id,
-        username: data.username,
-        duration: lastExercise.duration,
-        description: lastExercise.description,
-        date: lastExercise.date.toDateString(),
-      };
-      res.json(ans);
+      res.json(data);
     }
   );
 });
 
 app.get("/api/users/:_id/logs", (req, res) => {
-  console.log("\nAsked for exercise records: id=" + req.params._id);
-  console.log(req.query);
-  let start = req.query.from,
-    end = req.query.to,
-    limit = req.query.limit;
-  User.findById(req.params._id, (err, data) => {
-    if (err) return console.log(err);
-    if (data) {
-      // gather relevant info into an object
-      const ans = {
-        _id: req.params._id,
-        username: data.username,
-        count: 0,
-        log: [],
-      };
-      // filter log
-      let log = data.exercises.slice();
-      // remove too-early entries
-      if (start) {
-        start = new Date(start);
-        log = log.filter((item) => {
-          return item.date >= start;
-        });
-      }
-      // remove too-late entries
-      if (end) {
-        end = new Date(end);
-        log = log.filter((item) => {
-          return item.date <= end;
-        });
-      }
-      // sort
-      log.sort((a, b) => {
-        if (a.date > b.date) return 1;
-        if (a.date < b.date) return -1;
-        return 0;
-      });
-      // limit length
-      if (limit) {
-        log = log.slice(0, limit);
-      }
-      // map data to expected output
-      log = log.map((item) => {
-        return {
-          description: item.description,
-          duration: item.duration,
-          date: item.date.toDateString(),
-        };
-      });
-      // add log & count to answer
-      ans.log = log;
-      ans.count = log.length;
-
-      return res.json(ans);
-    }
-  });
+  // console.log("\nAsked for exercise records: id=" + req.params._id);
+  // console.log(req.query);
+  // let start = req.query.from,
+  //   end = req.query.to,
+  //   limit = req.query.limit;
+  // User.findById(req.params._id, (err, data) => {
+  //   if (err) return console.log(err);
+  //   if (data) {
+  //     // gather relevant info into an object
+  //     const ans = {
+  //       _id: req.params._id,
+  //       username: data.username,
+  //       count: 0,
+  //       log: [],
+  //     };
+  //     // filter log
+  //     let log = data.exercises.slice();
+  //     // remove too-early entries
+  //     if (start) {
+  //       start = new Date(start);
+  //       log = log.filter((item) => {
+  //         return item.date >= start;
+  //       });
+  //     }
+  //     // remove too-late entries
+  //     if (end) {
+  //       end = new Date(end);
+  //       log = log.filter((item) => {
+  //         return item.date <= end;
+  //       });
+  //     }
+  //     // sort
+  //     log.sort((a, b) => {
+  //       if (a.date > b.date) return 1;
+  //       if (a.date < b.date) return -1;
+  //       return 0;
+  //     });
+  //     // limit length
+  //     if (limit) {
+  //       log = log.slice(0, limit);
+  //     }
+  //     // map data to expected output
+  //     log = log.map((item) => {
+  //       return {
+  //         description: item.description,
+  //         duration: item.duration,
+  //         date: item.date.toDateString(),
+  //       };
+  //     });
+  //     // add log & count to answer
+  //     ans.log = log;
+  //     ans.count = log.length;
+  //     return res.json(ans);
+  //   }
+  // });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
